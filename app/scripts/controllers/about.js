40235c69f391f5c6f23fce78389fb7ce7f680c13
Yash Shah <yash9414@gmail.com>
2015-03-30 16:38:19 +0530

    Added basic Angular structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name panipuriApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the panipuriApp
+ */
+angular.module('panipuriApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name panipuriApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the panipuriApp
+ */
+angular.module('panipuriApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
